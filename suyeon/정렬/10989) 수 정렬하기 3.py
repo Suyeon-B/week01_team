256ca291afeac580acd551f43bab5fe7790e20e8
Suyeon-B <tndus4243@naver.com>
2022-04-02 11:52:02 +0900

    정렬 마지막 문제 푸는 중

--- suyeon/정렬/10989) 수 정렬하기 3.py
+++ suyeon/정렬/10989) 수 정렬하기 3.py
@@ -1,10 +1,13 @@
 # 10989
+# pypy3가 python3보다 빨라도, 메모리는 더 잡아먹는다는 사실....
+# 어떻게 알았냐구요? 저도 알고 싶지 않았어요 ㅎㅎ 하하
 import sys
-n = int(sys.stdin.readline())
-numbers = []
-for i in range(n):
-    numbers.append(int(input()))
+n = sys.stdin.readline()
+numbers = [0] * 10001
+for i in range(int(n)):
+    numbers[int(sys.stdin.readline())] += 1
 
-numbers.sort()
-for i in range(n):
-    print(numbers[i])
+for i in range(10001):
+    if numbers[i] != 0:
+        for j in range(numbers[i]):
+            print(i)
